156ea461dd3a846b2f14a7800b5b49d3e471604e
Kima <kimavideos97@gmail.com>
2023-08-22 19:52:42 +0200

    finished index page

--- src/main/layout.tsx
+++ src/main/layout.tsx
@@ -1,11 +1,17 @@
+import { useRef } from "react";
 import NavBar from "./components/navbar";
 import MainIndex from "./pages";
 
 const MainLayout = () => {
+    const downloadRef = useRef<null | HTMLDivElement>(null);
+    const scrollToDownload = () => {
+        downloadRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+
     return (
         <div className="select-none flex flex-col w-full h-full bg-[#0F131D]">
-            <NavBar />
-            <MainIndex />
+            <NavBar scrollToDownload={scrollToDownload} />
+            <MainIndex scrollToDownload={scrollToDownload} downloadRef={downloadRef} />
         </div>
     )
 }
--- src/main/pages/index.tsx
+++ src/main/pages/index.tsx
@@ -6,8 +6,9 @@ import { useEffect, useState } from 'react';
 import premFeature1 from '/image/premium_1.png?url';
 import premFeature2 from '/image/premium_2.png?url';
 import lockIcon from '/image/icon/lock.svg?url';
+import circleLogo from '/image/brand/logo.png?url';
 
-const MainIndex = () => {
+const MainIndex = ({ scrollToDownload, downloadRef }: { scrollToDownload: any, downloadRef: any }) => {
     const [latestVersion, setLatestVersion] = useState<any>('');
 
     const fetchLatestVersion = async () => {
@@ -17,7 +18,7 @@ const MainIndex = () => {
 
     useEffect(() => {
         fetchLatestVersion();
-    }, []);
+    }, []);    
 
     return (
         <div className="flex flex-col w-full h-full items-center justify-center">
@@ -28,7 +29,7 @@ const MainIndex = () => {
                 <div className='flex flex-col items-center justify-center gap-3 drop-shadow-[0_0_100px_rgba(27,58,117,0.5)]'>
                     <img src={fullLogo} alt="iphone mockup" width={400}/>
                     <p className='font-bold text-[34px] text-center'>Egy nem hivatalos e-KRÉTA kliens</p>
-                    <Link to={'#download'} className='rounded-full'>
+                    <Link to={''} onClick={scrollToDownload} className='rounded-full'>
                         <div className='flex flex-row items-center justify-center py-3 px-10 bg-gradient-to-r from-[#3C7BF5] to-[#3C5AF5] rounded-full'>
                             <p className='text-[34px] font-medium'>
                                 <span className='font-extrabold'>Letöltés</span> | v{latestVersion}
@@ -44,7 +45,7 @@ const MainIndex = () => {
                     <img src={iPhoneMockup2} alt="iphone mockup" width={1000}/>
                 </div>
             </div>
-            <div className='flex items-center justify-center px-10 w-full h-max -skew-y-2 bg-[#0F131D] mt-[-20px] pb-14'>
+            <div className='flex items-center justify-center px-10 w-full h-max -skew-y-2 bg-[#0F131D] mt-[-20px] pb-20'>
                 <div className='flex flex-col items-center justify-center px-10 pt-12 w-full w-full h-max skew-y-2'>
                     <h1 className='font-bold text-[65px] mt-8 text-center'>Ingyenes, örökké</h1>
                     <p className='text-[25px] leading-3 text-center'>Az összes Filc Prémium funckiót <span className='bg-clip-text bg-gradient-to-r from-[#3C5AF5] to-[#3C7BF5] text-fill-transparent'>ingyenessé</span> tettük.</p>
@@ -68,7 +69,7 @@ const MainIndex = () => {
                     <p className='font-extralight text-[25px] mt-6'>és még sok más...</p>
                 </div>
             </div>
-            <div className='flex items-center justify-center px-10 w-full h-max -skew-y-2 bg-[#000000] py-24'>
+            <div className='flex items-center justify-center px-10 w-full h-max -skew-y-2 bg-black py-24'>
                 <div className='flex flex-col items-center justify-center px-10 w-full w-full h-max skew-y-2'>
                     <h2 className='font-bold text-[30px]'>Nem sok adatot tárolunk rólad.</h2>
                     <p className='text-[25px]'>
@@ -80,10 +81,10 @@ const MainIndex = () => {
                     </p>
                 </div>
             </div>
-            <div className='flex items-center justify-center px-10 w-full h-max -skew-y-2 bg-[#0F131D] pb-14'>
-                <div className='flex flex-col items-center justify-center px-10 pt-12 w-full w-full h-max skew-y-2' id='download'>
+            <div className='flex items-center justify-center px-10 w-full h-max -skew-y-2 bg-[#0F131D] pb-20' ref={downloadRef}>
+                <div className='flex flex-col items-center justify-center px-10 pt-12 w-full w-full h-max skew-y-2'>
                     <h1 className='font-bold text-[65px] mt-8 text-center'>Meggyőztünk?</h1>
-                    <p className='text-[25px] leading-3 text-center'>Töltsd le most! <span className='bg-clip-text bg-gradient-to-r from-[#FFFFFF] to-transparent text-fill-transparent'>(or else..)</span></p>
+                    <p className='text-[25px] leading-3 text-center'>Töltsd le most! <span className='bg-clip-text bg-gradient-to-r from-white to-transparent text-fill-transparent'>(or else..)</span></p>
                     <h2 className='font-bold text-[25px] mt-8'>Verzió: {latestVersion}</h2>
                     <div className='flex flex-col items-start justify-center gap-6 mt-6'>
                         <div className='flex flex-row items-center justify-start gap-14'>
@@ -120,8 +121,41 @@ const MainIndex = () => {
                             </p>
                         </div>
                     </div>
+                    <Link to={'/go/s/github-releases'} className='mt-6'>
+                        <p className='font-extralight text-[25px]'>régebbi verziók...</p>
+                    </Link>
                 </div>
             </div>
+            <div className='flex items-center justify-center px-10 w-full h-max -skew-y-2 bg-black py-24'>
+                <div className='flex flex-row items-center justify-between px-10 w-full w-full h-max skew-y-2'>
+                    <div className='flex flex-col items-start justify-center gap-2'>
+                        <div className='flex flex-row items-center justify-start gap-5'>
+                            <img src={circleLogo} alt="logo" width={80} height={80}/>
+                            <h1 className='font-extrabold text-[75px]'>reFilc</h1>
+                        </div>
+                        <h2 className='font-bold text-[30px] text-[#B7B7B7]'>
+                            Website design by 
+                            <span className='underline ml-[10px]'> 
+                                <Link to={'https://liba.lol'} target='_blank'>liba</Link>
+                            </span>
+                            , developed by 
+                            <span className='underline mx-[10px]'> 
+                                <Link to={'https://vrolandd.hu'} target='_blank'>vrolandd</Link>
+                            </span> 
+                            and 
+                            <span className='underline ml-[10px]'> 
+                                <Link to={'https://kima.dev'} target='_blank'>kima</Link>
+                            </span>
+                            .<br />
+                            Copyright © 2023, All Rights Reserved.
+                        </h2>
+                        <Link to={'/privacy-policy'}>
+                            <p className='text-[20px] underline mt-8'>Adatkezelési tájékoztató</p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className='h-[30px] w-full bg-black mt-[-100px]'></div>
         </div>
     )
 }
