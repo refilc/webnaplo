3c392bc80a6412cc52315fee98f5e752b6a051e3
Kima <kimavideos97@gmail.com>
2024-01-21 17:47:27 +0100

    temp shit

--- src/ui/timeline/components/navbar.tsx
+++ src/ui/timeline/components/navbar.tsx
@@ -1,7 +1,7 @@
-import { Link, useLocation } from "react-router-dom";
+// import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
-    const location = useLocation();
+    // const location = useLocation();
 
     return (
         <div className="top-0 left-0 right-0 w-full flex flex-row items-center justify-between px-4 pt-4 z-50">
