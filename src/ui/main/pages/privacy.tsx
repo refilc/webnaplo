c1a20afe2aebb5c5dafc4fcd65a7b09f49c1d4d2
zypherift <regokoppany@gmail.com>
2024-10-24 20:11:42 +0200

    prettier date

--- src/ui/main/pages/privacy.tsx
+++ src/ui/main/pages/privacy.tsx
@@ -6,7 +6,7 @@ const PrivacyPolicy = () => {
         <div className="flex flex-col w-full h-full items-center justify-center">
             <div className="flex flex-col items-center justify-center px-10 pb-16 w-full">
                 <h1 className='font-bold text-[45px] mt-16'>Adatkezelési Irányelvek</h1>
-                <p className='text-[20px] opacity-70'>Utoljára módosítva: 2024. 10. 24.</p>
+                <p className='text-[20px] opacity-70'>Utoljára módosítva: Október 24, 2024</p>
                 <div className='flex flex-col gap-4 max-w-[900px] mt-10 text-justify indent-2'>
                     <p>• A reFilc (továbbiakban alkalmazás) egy mobilos, asztali és webes kliensalkalmazás, segítségével az e-KRÉTA rendszeréből letöltheted és felhasználóbarát módon megjelenítheted az adataidat. Tanulmányi adataid csak közvetlenül az alkalmazás és a KRÉTA-szerverek között közlekednek, titkosított kapcsolaton keresztül.</p>
                     <p>
