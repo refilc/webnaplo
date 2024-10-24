5641d76a18c47d3b06f85d483745dfd27af5ab2f
BalazsManus <contact@balazsmanus.hu>
2024-10-24 19:18:01 +0200

    rename to privacy policy

--- src/ui/main/pages/privacy.tsx
+++ src/ui/main/pages/privacy.tsx
@@ -5,7 +5,7 @@ const PrivacyPolicy = () => {
     return (
         <div className="flex flex-col w-full h-full items-center justify-center">
             <div className="flex flex-col items-center justify-center px-10 pb-16 w-full">
-                <h1 className='font-bold text-[45px] mt-16'>Adatkezelési tájékoztató</h1>
+                <h1 className='font-bold text-[45px] mt-16'>Adatvédelmi Irányelvek</h1>
                 <p className='text-[20px] opacity-70'>Utoljára módosítva: 2024. 02. 13.</p>
                 <div className='flex flex-col gap-4 max-w-[900px] mt-10 text-justify indent-2'>
                     <p>• A reFilc (továbbiakban alkalmazás) egy mobilos, asztali és webes kliensalkalmazás, segítségével az e-KRÉTA rendszeréből letöltheted és felhasználóbarát módon megjelenítheted az adataidat. Tanulmányi adataid csak közvetlenül az alkalmazás és a KRÉTA-szerverek között közlekednek, titkosított kapcsolaton keresztül.</p>
