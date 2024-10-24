1657b71be6362c7b5dbe97a7eaa98dfd7b71153b
Kima <kimavideos97@gmail.com>
2024-10-24 21:24:12 +0200

    uwu

--- src/ui/main/pages/privacy.tsx
+++ src/ui/main/pages/privacy.tsx
@@ -5,8 +5,8 @@ const PrivacyPolicy = () => {
     return (
         <div className="flex flex-col w-full h-full items-center justify-center">
             <div className="flex flex-col items-center justify-center px-10 pb-16 w-full">
-                <h1 className='font-bold text-[45px] mt-16'>Adatkezelési Irányelvek</h1>
-                <p className='text-[20px] opacity-70'>Utoljára módosítva: Október 24, 2024</p>
+                <h1 className='font-bold text-[45px] mt-16'>Adatkezelési Tájékoztató</h1>
+                <p className='text-[20px] opacity-70'>Utoljára módosítva: 2024. 10. 24.</p>
                 <div className='flex flex-col gap-4 max-w-[900px] mt-10 text-justify indent-2'>
                     <p>• A reFilc (továbbiakban alkalmazás) egy mobilos, asztali és webes kliensalkalmazás, segítségével az e-KRÉTA rendszeréből letöltheted és felhasználóbarát módon megjelenítheted az adataidat. Tanulmányi adataid csak közvetlenül az alkalmazás és a KRÉTA-szerverek között közlekednek, titkosított kapcsolaton keresztül.</p>
                     <p>
