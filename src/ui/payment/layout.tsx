14c26c7eba099887a7239ad73955ad0cafab4890
Marton Kiss <kima.public@gmail.com>
2025-03-02 07:53:28 +0100

    uwu

--- src/ui/payment/layout.tsx
+++ src/ui/payment/layout.tsx
@@ -1,7 +1,9 @@
 import { ReactPayPalScriptOptions, PayPalScriptProvider } from "@paypal/react-paypal-js";
-import PaymentPaypalMobileCheckout from "./pages/paypal/mobile-checkout";
+// import PaymentPaypalMobileCheckout from "./pages/paypal/mobile-checkout";
 
 const PaymentLayout = ({ currentPage }: { currentPage: string }) => {
+    console.log('currentPage', currentPage);
+
     const initialOptions: ReactPayPalScriptOptions = {
         clientId: "ATwZS8ntp-Z_2dHNrQRfZADStvvkJcawqCtvRS6NV8m02LMSkWU-jeQTaJwjQwII6mhGOAmCTV2lSQYm",
         locale: 'hu_HU',
@@ -13,10 +15,10 @@ const PaymentLayout = ({ currentPage }: { currentPage: string }) => {
     return (
         <div className="select-none flex flex-col w-full h-full bg-[#0F131D]">
             <PayPalScriptProvider options={initialOptions}>
-                {
+                {/* {
                     currentPage == 'pp-mobile-checkout' ? <PaymentPaypalMobileCheckout /> : 
                     ''
-                }
+                } */}
             </PayPalScriptProvider>
         </div>
     )
